b18b3e65b26c6bdf578ead722c12313b3bca013c
kirimlimustafa <mustafa.kirimli@markafoni.com>
2013-07-04 14:12:11 +0300

    initial post datası, anasayfa post

--- views.py
+++ views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from post.models import Post
 
 def home(request):
-    return render(request, 'home.html')
+    post = Post()
+    latest_posts = post.getLatestPost()
+    #TODO: check count
+    main_post = post.getMainPost()[0] if post.getMainPost() else None
+    return render(request, 'home.html', {'latest_posts': latest_posts, 'main_post': main_post})
