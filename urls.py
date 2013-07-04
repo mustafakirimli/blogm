4df2d879b7986aca1d844d4d394043c9c3b3d8c5
kirimlimustafa <mustafa.kirimli@markafoni.com>
2013-07-04 11:28:43 +0300

    django flatpage aktif edildi

--- urls.py
+++ urls.py
@@ -14,4 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # django filatpages
+    url(r'^pages/', include('django.contrib.flatpages.urls')),
 )
