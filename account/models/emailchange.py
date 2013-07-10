dd3042f82808c4dd348318b92f24f338e1c84d29
kirimlimustafa <mustafa.kirimli@markafoni.com>
2013-07-10 13:30:44 +0300

    fix

--- account/models/emailchange.py
+++ account/models/emailchange.py
@@ -8,23 +8,40 @@ from django.template import Context
 from django.utils.translation import ugettext as _
 
 class EmailChange(models.Model):
+    # user
     user = models.ForeignKey(User)
+    
+    # new email address
     email = models.CharField(max_length=75)
-    activation_key =  models.CharField(max_length=30)
+    
+    # email change requests activation key
+    activation_key = models.CharField(max_length=30)
+    
+    # email change request status 
+    # True: pending request
+    # False: approved request
     is_active = models.BooleanField(default=False)
+
+    # email change request created time
     created_at = models.DateTimeField(auto_now_add=True)
 
     @staticmethod
     def pending_requests(user):
+        """
+        returns active -pending- email change requests for given user
+        """
         change = EmailChange.objects.filter(user=user, is_active=True)
         return change[0] if change else False
 
     @task
     def send_activation_email(self):
+        """
+        send email change confirmation email to new address.
+        """
         site = Site.objects.get_current()
         # send email with template
         send_mail(
-            _('Please confirm email change!'),
+            _('Please confirm email change request!'),
             get_template('email/account/email_activate.html').render(
                 Context({
                     'site': site,
