5b2c33270a31b5cc0a4b13029b35be7e2235c37d
kirimlimustafa <mustafa.kirimli@markafoni.com>
2013-07-11 17:04:15 +0300

    form auto focus added

--- post/templatetags/app_tags.py
+++ post/templatetags/app_tags.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.filter(name='md5')
 def md5_string(value):
     return hashlib.md5(value).hexdigest()
+
+@register.filter(name='get_hash')
+def get_hash(value):
+    try:
+        return value.__hash__()
+    except Exception, e:
+        print e
+        return None
