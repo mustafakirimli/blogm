7b927140601078fd451d2a818dbc01c5115266aa
kirimlimustafa <mustafa.kirimli@markafoni.com>
2013-07-15 13:18:19 +0300

    pep8

--- views.py
+++ views.py
@@ -5,7 +5,10 @@ def home(request):
     latest_posts = Post.get_latest_post()
     #TODO: check count
     main_post = Post.get_main_post()[0] if Post.get_main_post() else None
-    return render(request, 'home.html', {'latest_posts': latest_posts, 'main_post': main_post})
+    return render(request, 'home.html', {
+    	'latest_posts': latest_posts, 
+    	'main_post': main_post
+    })
 
 def wrong(request):
 	pass
