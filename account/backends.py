190c3d8c527cfd2f35db096f6a8672a6a88f03c4
kirimlimustafa <mustafa.kirimli@markafoni.com>
2013-07-18 09:11:34 +0300

    kullanıcı is_active ve approval status kontrolü eklendi

--- account/backends.py
+++ account/backends.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.models import User, check_password
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext as _
 
 class EmailAuthBackend(object):
     """
@@ -7,13 +9,14 @@ class EmailAuthBackend(object):
     
     def authenticate(self, username=None, password=None):
         """ Authenticate a user based on email address as the user name. """
-        try:
-            user = User.objects.get(email=username)
-            if user.check_password(password):
-                return user
-        except User.DoesNotExist:
-            return None
-        return None
+        user = User.objects.get(email=username)
+        if not user.check_password(password):
+            raise ValidationError(_("Invalid email/password"))
+        if not user.is_active:
+            raise ValidationError(_("Disabled account. Please contact blog admin"))
+        if not user.get_profile().is_approved:
+            raise ValidationError(_("Not activated account! Please activate your account"))
+        return user
 
     def get_user(self, user_id):
         """ Get a User object from the user_id. """
