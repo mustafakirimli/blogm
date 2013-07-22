1d228d0fb8f7d36ad952fce64fa22df23a09bcd0
kirimlimustafa <mustafa.kirimli@markafoni.com>
2013-07-22 10:13:49 +0300

    post comment template cache

--- post/utils.py
+++ post/utils.py
@@ -1,4 +1,4 @@
-from utils import purge_template_cache
+from blogm.utils import purge_template_cache
 
 def purge_comment_cache(post_id):
     purge_template_cache(cache_name="comment", identify=post_id)
