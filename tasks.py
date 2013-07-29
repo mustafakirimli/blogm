ec2527a8cde0c9c87e5cace94fbb008e2aa24420
= <=>
2013-07-29 11:58:43 +0300

    homepage cache purge task changed

--- tasks.py
+++ tasks.py
@@ -1,9 +1,11 @@
 from django.db.models.signals import post_save
 from django.core.urlresolvers import reverse
+from django.dispatch import receiver
 
 from blogm.utils import purge_url_cache, purge_cache
 from post.models import Post
 
+@receiver(post_save, sender=Post)
 def purge_homepage_cache(sender, **kwargs):
     #post = kwargs["instance"]
     
@@ -11,5 +13,3 @@ def purge_homepage_cache(sender, **kwargs):
     purge_url_cache(reverse("home"))
     purge_cache("latest_posts")
     purge_cache("main_post")
-
-post_save.connect(purge_homepage_cache, sender=Post, dispatch_uid="pasphc")
