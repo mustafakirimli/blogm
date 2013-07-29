c2d7e79240a6d5a6a66e5be03eecc5e688f47275
= <=>
2013-07-29 11:43:57 +0300

    homepage cache purge fix

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import post_save
 from django.core.urlresolvers import reverse
 
 from blogm.utils import purge_url_cache, purge_cache
@@ -12,5 +12,4 @@ def purge_homepage_cache(sender, **kwargs):
     purge_cache("latest_posts")
     purge_cache("main_post")
 
-pre_save.connect(purge_homepage_cache, sender=Post, dispatch_uid="pbsphc")
 post_save.connect(purge_homepage_cache, sender=Post, dispatch_uid="pasphc")
